cf363b010b230c61665480748bd476d94e6efdc0
Ferhad <qayibovferhad04@gmail.com>
2025-01-13 12:36:31 +0400

    admin categories and comments added

--- admin/js/main.js
+++ admin/js/main.js
@@ -1,29 +1,51 @@
 // MODAL START //
-let deleteBtn = document.querySelector(".delete-btn")
+let deleteBtn = document.querySelectorAll(".delete-btn")
 let deleteModal = document.querySelector("#deleteModal")
 
-let editBtn = document.querySelector(".edit-btn")
+let editBtn = document.querySelectorAll(".edit-btn")
 let editModal = document.querySelector("#editModal")
 
 let createBtn = document.querySelector(".create-btn")
 let createModal = document.querySelector("#createModal")
 
+const closeBtn = document.querySelectorAll(".close")
+
 let delCloseModalBtn = document.querySelector(".del-modal-close"), editModalCloseBtn = document.querySelector(".edit-modal-close")
 
-deleteBtn.addEventListener("click", function (event) {
-    deleteModal.classList.add("show")
+deleteBtn.forEach(btn=>{
+    btn.addEventListener("click", function (event) {
+        deleteModal.classList.add("show")
+    })
 })
 
-createBtn.addEventListener("click", function (event) {
-    createModal.classList.add("show")
+if(createBtn){
+    createBtn.addEventListener("click", function (event) {
+        createModal.classList.add("show")
+    })
+}
+
+
+editBtn.forEach(btn=>{
+    btn.addEventListener("click", function (event) {
+        editModal.classList.add("show")
+    })
 })
 
-editBtn.addEventListener("click", function (event) {
-    editModal.classList.add("show")
+
+closeBtn.forEach(btn=>{
+    btn.addEventListener("click", function (event) {
+        let modal = event.target.closest(".modal")
+        modal.classList.remove("show")
+    })
 })
+
+
+console.log(delCloseModalBtn);
+
 
 delCloseModalBtn.addEventListener("click", function (event) {
     let modal = event.target.closest(".modal")
+    console.log(modal);
     modal.classList.remove("show")
 })
 // MODAL END //
